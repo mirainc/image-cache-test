fdfff62d0bfba0af6305a4f4e408be3f6a6ff484
Jordan <github@jschr.io>
2024-11-01 15:24:16 -0400

    Use same NextJS version as Gotts

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function Home() {
+  const router = useRouter();
+
+  // Reload the page to check for an updated image.
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace(router.asPath.split("#")[0]);
+    }, 5000);
+  }, [router]);
+
+  return <main style={{ backgroundImage: 'url("/image6.jpg")' }}></main>;
+}
