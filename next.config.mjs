0dd59ec80a7dee4bcfd6d72aad38d56a9d0f06c5
Jordan <github@jschr.io>
2024-11-01 15:53:13 -0400

    Set s-maxage header

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
         headers: [
           {
             key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
+            value: "public, max-age=0, s-maxage=31536000 immutable",
           },
         ],
         source:
