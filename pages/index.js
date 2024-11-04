d5ee6df072eef34b3f8753ba0660e81970c75346
Jordan <github@jschr.io>
2024-11-04 10:45:05 -0500

    New images

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,17 @@ export default function Home() {
   const router = useRouter();
 
   // Reload the page to check for an updated image.
-  useEffect(() => {
-    setTimeout(() => {
-      router.replace(router.asPath.split("#")[0]);
-    }, 5000);
-  }, [router]);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     router.replace(router.asPath.split("#")[0]);
+  //   }, 5000);
+  // }, [router]);
 
-  return <main style={{ backgroundImage: 'url("/image12.jpg")' }}></main>;
+  return (
+    <main>
+      <div style={{ backgroundImage: 'url("/image17.jpg")' }} />
+      <div style={{ backgroundImage: 'url("/image18.jpg")' }} />
+      <div style={{ backgroundImage: 'url("/image19.jpg")' }} />
+    </main>
+  );
 }
