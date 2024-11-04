4298c5708fcb023a81298f66e35afc10bf041da0
Jordan <github@jschr.io>
2024-11-04 09:59:24 -0500

    Remove cache control headers from public assets

--- next.config.mjs
+++ next.config.mjs
@@ -2,16 +2,16 @@
 const nextConfig = {
   async headers() {
     return [
-      {
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=0, s-maxage=31536000 immutable",
-          },
-        ],
-        source:
-          "/:path(.+\\.(?:ico|png|svg|jpg|jpeg|avif|gif|webp|json|js|css|mp3|mp4|ttf|ttc|otf|woff|woff2)$)",
-      },
+      // {
+      //   headers: [
+      //     {
+      //       key: "Cache-Control",
+      //       value: "public, max-age=0, s-maxage=31536000 immutable",
+      //     },
+      //   ],
+      //   source:
+      //     "/:path(.+\\.(?:ico|png|svg|jpg|jpeg|avif|gif|webp|json|js|css|mp3|mp4|ttf|ttc|otf|woff|woff2)$)",
+      // },
     ];
   },
 };
